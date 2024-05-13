9d8e578f5053a23b4fd04be68b7fa6a16cece2cb
Pablo Piffer <pablopiffer@gmail.com>
2024-05-12 23:23:00 -0300

    feature: Crud ok

--- Porquepsi/Tratamiento/models.py
+++ Porquepsi/Tratamiento/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Institucion(models.Model):
-    nombre = models.CharField(max_length=255)
+    nombre = models.CharField(max_length=255, unique=True)
 
     def __str__(self):
         return self.nombre
