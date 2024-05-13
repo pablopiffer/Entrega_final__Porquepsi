7941cbdcf8053eadb8b44e645c1a0ae6850087bb
Pablo Piffer <pablopiffer@gmail.com>
2024-05-13 00:05:24 -0300

    feat: se agregó urls y templates

--- Porquepsi/Porquepsi/urls.py
+++ Porquepsi/Porquepsi/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    #path("", include("core.urls")),
-    #path("Tratamiento/", include("Tratamiento.urls")),
+    path("", include("core.urls")),
+    path("Tratamiento/", include("Tratamiento.urls")),
 ]
--- Porquepsi/Tratamiento/views.py
+++ Porquepsi/Tratamiento/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+def index(request):
+    consulta = models.TipoDeConsulta.objects.all()
+    contexto = {"tipos" : consulta}
+    return render(request, "Tratamiento/index.html", contexto)
