7027cf40869cbbda61bcda373be9887cd09d1456
Pablo Piffer <pablopiffer@gmail.com>
2024-05-14 01:28:36 -0300

    feat:pruebas de CRUD

--- Porquepsi/Tratamiento/views.py
+++ Porquepsi/Tratamiento/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 
-#from producto.forms import ProductoCategoriaForm
+from Tratamiento.forms import TratamientopacienteForm
 from .models import Paciente
 
 
@@ -17,3 +17,39 @@ def lista_pacientes(request):
         consulta = Paciente.objects.all()
     contexto = {"Paciente": consulta}
     return render(request, "Tratamiento/lista_pacientes.html", contexto)
+
+def paciente_create(request):
+    if request.method == "POST":
+        form = TratamientopacienteForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("Tratamiento:lista_pacientes")
+    else:  # GET
+        form = TratamientopacienteForm()
+    return render(request, "Tratamiento/paciente_form.html", {"form": form})
+
+
+def paciente_detalles(request, pk: int):
+    consulta = Paciente.objects.get(id=pk)
+    contexto = {"Paciente": consulta}
+    return render(request, "Tratamiento/paciente_detalles.html", contexto)
+
+
+def paciente_update(request, pk: int):
+    consulta = Paciente.objects.get(id=pk)
+    if request.method == "POST":
+        form = TratamientopacienteForm(request.POST, instance=consulta)
+        if form.is_valid():
+            form.save()
+            return redirect("Tratamiento:lista_pacientes")
+    else:  # GET
+        form = TratamientopacienteForm(instance=consulta)
+    return render(request, "Tratamiento/paciente_form.html", {"form": form})
+
+
+def paciente_delete(request, pk: int):
+    consulta = Paciente.objects.get(id=pk)
+    if request.method == "POST":
+        consulta.delete()
+        return redirect("Tratamiento:lista_pacientes")
+    return render(request, "Tratamiento/paciente_confirm_delete.html", {"object": consulta})
