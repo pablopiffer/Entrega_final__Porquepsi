81a8ff8fc1ea6ed132cbcc6779ba9bc5aa9d1916
Pablo Piffer <pablopiffer@gmail.com>
2024-05-25 20:36:41 -0300

    feat: cambio a vistas basadas en Clases

--- Porquepsi/Tratamiento/urls.py
+++ Porquepsi/Tratamiento/urls.py
@@ -1,21 +1,30 @@
 from django.urls import path
 
 from Tratamiento.views import (
+    PacienteList,
+    PacienteCreate,
+    PacienteDelete,
+    PacienteDetail,
+    PacienteUpdate,
     index,
-    lista_pacientes,
-    paciente_create,
-    paciente_delete,
-    paciente_detalles,
-    paciente_update,
+
 )
 
 app_name = "Tratamiento"
 
 urlpatterns = [
-    path("", index, name="index"),
-    path("lista_pacientes/", lista_pacientes, name="lista_pacientes"),
-    path("paciente_confirm_delete/<int:pk>", paciente_delete, name="paciente_confirm_delete"),
-    path("paciente_detalles/<int:pk>", paciente_detalles, name="paciente_detalles"),
-    path("paciente_update/<int:pk>", paciente_update, name="paciente_update"),
-    path("paciente_form/", paciente_create, name="paciente_form"),
+    path("", index, name="index")
+    # path("lista_pacientes/", lista_pacientes, name="lista_pacientes"),
+    # path("paciente_confirm_delete/<int:pk>", paciente_delete, name="paciente_confirm_delete"),
+    # path("paciente_detalles/<int:pk>", paciente_detalles, name="paciente_detalles"),
+    # path("paciente_update/<int:pk>", paciente_update, name="paciente_update"),
+    # path("paciente_form/", paciente_create, name="paciente_form"),
+]
+
+urlpatterns += [
+    path("Tratamiento/lista_pacientes", PacienteList.as_view(), name="lista_pacientes"),
+    path("Tratamiento/paciente_form/", PacienteCreate.as_view(), name="paciente_form"),
+    path("Tratamiento/paciente_detalles/<int:pk>", PacienteDetail.as_view(), name="paciente_detalles"),
+    path("paciente_update/<int:pk>", PacienteUpdate.as_view(), name="paciente_update"),
+    path("Tratamiento/paciente_confirm_delete/<int:pk>", PacienteDelete.as_view(), name="paciente_confirm_delete"),
 ]
--- Porquepsi/Tratamiento/views.py
+++ Porquepsi/Tratamiento/views.py
@@ -1,3 +1,17 @@
+from typing import Any
+
+from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.shortcuts import redirect, render
+from django.urls import reverse_lazy
+from django.views.generic import (
+    CreateView,
+    DeleteView,
+    DetailView,
+    ListView,
+    UpdateView,
+)
+
 from django.shortcuts import redirect, render
 
 from Tratamiento.forms import TratamientopacienteForm
@@ -8,47 +22,79 @@ def index(request):
     return render(request, "Tratamiento/index.html")
 
 
-def lista_pacientes(request):
+""" def lista_pacientes(request):
     busqueda = request.GET.get("busqueda", None)
     if busqueda:
         consulta = Paciente.objects.filter(nombre__icontains=busqueda)
     else:
         consulta = Paciente.objects.all()
     contexto = {"paciente": consulta}  
-    return render(request, "Tratamiento/lista_pacientes.html", contexto)
+    return render(request, "Tratamiento/lista_pacientes.html", contexto) """
 
-def paciente_create(request):
-    if request.method == "POST":
-        form = TratamientopacienteForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect("Tratamiento:lista_pacientes")
-    else:  # GET
-        form = TratamientopacienteForm()
-    return render(request, "Tratamiento/paciente_form.html", {"form": form})
+class PacienteList(ListView):
+    model = Paciente
+    context_object_name = "paciente"
+    template_name = "Tratamiento/lista_pacientes.html"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        busqueda = self.request.GET.get("busqueda")
+        if busqueda:
+            queryset = queryset.filter(
+                Q(nombre__icontains=busqueda) | Q(profesional__nombre__icontains=busqueda)
+            )
+        return queryset
+
+# def paciente_create(request):
+#     if request.method == "POST":
+#         form = TratamientopacienteForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("Tratamiento:lista_pacientes")
+#     else:  # GET
+#         form = TratamientopacienteForm()
+#     return render(request, "Tratamiento/paciente_form.html", {"form": form})
+
+class PacienteCreate(CreateView):
+    model = Paciente
+    form_class = TratamientopacienteForm
+    success_url = reverse_lazy("Tratamiento:lista_pacientes")
 
 
-def paciente_detalles(request, pk: int):
-    consulta = Paciente.objects.get(id=pk)
-    contexto = {"Paciente": consulta}
-    return render(request, "Tratamiento/paciente_detalles.html", contexto)
+# def paciente_detalles(request, pk: int):
+#     consulta = Paciente.objects.get(id=pk)
+#     contexto = {"Paciente": consulta}
+#     return render(request, "Tratamiento/paciente_detalles.html", contexto)
+
+class PacienteDetail(DetailView):
+    model = Paciente
+    template_name = "Tratamiento/paciente_detalles.html"
 
 
-def paciente_update(request, pk: int):
-    consulta = Paciente.objects.get(id=pk)
-    if request.method == "POST":
-        form = TratamientopacienteForm(request.POST, instance=consulta)
-        if form.is_valid():
-            form.save()
-            return redirect("Tratamiento:lista_pacientes")
-    else:  # GET
-        form = TratamientopacienteForm(instance=consulta)
-    return render(request, "Tratamiento/paciente_form.html", {"form": form})
+# def paciente_update(request, pk: int):
+#     consulta = Paciente.objects.get(id=pk)
+#     if request.method == "POST":
+#         form = TratamientopacienteForm(request.POST, instance=consulta)
+#         if form.is_valid():
+#             form.save()
+#             return redirect("Tratamiento:lista_pacientes")
+#     else:  # GET
+#         form = TratamientopacienteForm(instance=consulta)
+#     return render(request, "Tratamiento/paciente_form.html", {"form": form})
+
+class PacienteUpdate(UpdateView):
+    model = Paciente
+    form_class = TratamientopacienteForm
+    success_url = reverse_lazy("Tratamiento:lista_pacientes")
 
 
-def paciente_delete(request, pk: int):
-    consulta = Paciente.objects.get(id=pk)
-    if request.method == "POST":
-        consulta.delete()
-        return redirect("Tratamiento:lista_pacientes")
-    return render(request, "Tratamiento/paciente_confirm_delete.html", {"object": consulta})
+# def paciente_delete(request, pk: int):
+#     consulta = Paciente.objects.get(id=pk)
+#     if request.method == "POST":
+#         consulta.delete()
+#         return redirect("Tratamiento:lista_pacientes")
+#     return render(request, "Tratamiento/paciente_confirm_delete.html", {"object": consulta})
+
+class PacienteDelete(DeleteView):
+    model = Paciente
+    success_url = reverse_lazy("Tratamiento:lista_pacientes")
