6b5302c85279c1442d9c19f1e37919f9721ce0c7
Pablo Piffer <pablopiffer@gmail.com>
2024-05-26 23:17:57 -0300

    feat: register

--- Porquepsi/core/urls.py
+++ Porquepsi/core/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 
-from core.views import CustomLoginView, index
+from core.views import CustomLoginView, index, register
 
 from . import views
 
@@ -12,6 +12,7 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
+    path("register/", register, name = "register")
 ]
 
 #urlpatterns += staticfiles_urlpatterns()
