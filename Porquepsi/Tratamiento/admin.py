053b381fa727d1c2b1dc4a6d561f484cefd0acd1
Pablo Piffer <pablopiffer@gmail.com>
2024-05-27 22:20:02 -0300

    feat: cambios en admin y agregado de filtros

--- Porquepsi/Tratamiento/admin.py
+++ Porquepsi/Tratamiento/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 
 from . import models
 
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'fecha_nacimiento', 'direccion', 'telefono', 'email', 'numero_historia_clinica')
+    list_display_links = ('nombre',) 
+    list_filter = ('profesional', 'institucion') 
+    search_fields = ('nombre', 'numero_historia_clinica')
+    date_hierarchy = 'fecha_nacimiento'  
+
+
 admin.site.register(models.Profesional)
-admin.site.register(models.Paciente)
+admin.site.register(models.Paciente, PacienteAdmin)
 admin.site.register(models.TipoDeConsulta)
 admin.site.register(models.Institucion)
