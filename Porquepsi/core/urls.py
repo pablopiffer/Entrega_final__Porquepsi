1bf59d934d77f9cceaf60f7633336725446c1579
Pablo Piffer <pablopiffer@gmail.com>
2024-05-30 22:53:00 -0300

    feat: creación de "nosotros"

--- Porquepsi/core/urls.py
+++ Porquepsi/core/urls.py
@@ -2,7 +2,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from django.contrib.auth.views import LogoutView
 
-from core.views import CustomLoginView, index, register
+from core.views import CustomLoginView, index, register, nosotros
 
 from . import views
 
@@ -12,7 +12,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("login/", CustomLoginView.as_view(), name="login"),
     path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
-    path("register/", register, name = "register")
+    path("register/", register, name = "register"),
+    path("nosotros/", nosotros, name="nosotros"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- Porquepsi/core/views.py
+++ Porquepsi/core/views.py
@@ -22,3 +22,6 @@ def register(request: HttpRequest) -> HttpResponse:
     else:
         form = CustomUserCreationForm()
     return render(request, "core/register.html", {"form": form})
+
+def nosotros(request):
+    return render(request, 'core/nosotros.html')
